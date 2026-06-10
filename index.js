06659708a426c88a84b188c1060d0d4b4f275892
Edualsena <eduedualsena12390@gmail.com>
2026-06-10 17:02:26 -0300

    update variables

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 const path = require('path');
 
+
+
 const app = express();
 
-mongoose.connect('mongodb+srv://eduedualsena12390_db_user:<ixHqcFio2GwEBxCV>@cluster0.wmgfgjo.mongodb.net/?appName=Cluster0').then(function(){
+const Posts = require('./Posts');
+
+mongoose.connect(process.env.MONGO_URI).then(function(){
     console.log("Conectado com sucesso");
 }).catch(function(err){
     console.log(err.message);
@@ -21,10 +26,13 @@ app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/pages'));
 
-
+/*
 app.get('/',(req,res)=>{
 
     if(req.query.busca == null){
+        Posts.find({}).sort({'_id':-1}).exec(function(err,posts){
+            console.log(posts[0]);
+        })
         res.render('home',{});    
     }else{
         res.render('busca',{});
@@ -32,6 +40,33 @@ app.get('/',(req,res)=>{
 
     
 })
+*/
+
+// Código Corrigido (com async/await)
+
+app.get('/', async (req, res) => {
+
+    if (req.query.busca == null) {
+        try {
+            // Removido o callback e o .exec(), adicionado o await
+            const posts = await Posts.find({}).sort({ '_id': -1 });
+            
+            if (posts.length > 0) {
+                console.log(posts[0]);
+            }
+            
+            // Se você precisa enviar os posts para a página, lembre-se de passá-los aqui:
+            res.render('home', { posts: posts }); 
+        } catch (err) {
+            console.error("Erro ao buscar posts:", err);
+            res.status(500).send("Erro interno do servidor");
+        }   
+    } else {
+        res.render('busca', {});
+    }
+
+});
+
 
 app.get('/:slug',(req,res)=>{
     //res.send(req.params.slug);
